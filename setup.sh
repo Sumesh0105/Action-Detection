203df6b4cf0de26fa769b66af323252faf87e319
Sumesh0105 <113231945+Sumesh0105@users.noreply.github.com>
2022-09-10 15:52:50 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
+email = \"vsumesh0105@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 headless = true\n\
-port = $PORT\n\
 enableCORS = false\n\
+port = $PORT\n\
 \n\
 " > ~/.streamlit/config.toml
+
+
