e620df10da799c0b9a4e18397f3d34c9bcc33eac
Sumesh0105 <113231945+Sumesh0105@users.noreply.github.com>
2022-09-11 20:17:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown("<h6 style='text-align: right; color: gray;'>~sumesh varadharajan</h
 run = st.checkbox('Click to Run')
 FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
-model2 = tf.keras.models.load_model('DeepVisionModel.h5')
+model2 = tf.keras.models.load_model('/Users/sumeshvaradharajan/Desktop/model/DeepVisionModel.h5')
 font = cv2.FONT_HERSHEY_SIMPLEX
 # org
 org = (80, 224)
@@ -22,23 +22,36 @@ color = (255, 0, 0)
 thickness = 3
 while run:
     return_value, frame1 = camera.read()
+    FRAME_WINDOW.image(frame1)
     time.sleep(0.001)
 
     return_value, frame2 = camera.read()
-    time.sleep(0.001)
+    FRAME_WINDOW.image(frame2)
 
+    time.sleep(0.001)
+else:
+    st.markdown("<h4 style='text-align: center; color: gray;'>Bye..</h4>", unsafe_allow_html=True)
+
+    
+
+"""
     cv2.imwrite("image1.png", frame1)
     image1 = cv2.imread("image1.png",0)
-
+    #image1 = cv2.cvtColor(frame1, cv2.IMREAD_COLOR)
+    #image1 = cv2.imdecode(frame1, cv2.IMREAD_COLOR)
     #image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image1 = cv2.resize(image1,(256,256))
     image1 = np.dstack([image1]*3)
     cv2.imwrite("image2.png", frame2)
     image2 = cv2.imread("image2.png",0)
-
+    #image2 = cv2.cvtColor(frame2, cv2.IMREAD_COLOR)
+    #image2 = cv2.imdecode(frame2, cv2.IMREAD_COLOR)
     #image2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
     image2 = cv2.resize(image2,(256,256))
     image2 = np.dstack([image2]*3)
+
+
+
 
     absdiff = cv2.absdiff(image1,image2)
     absdiff1 = np.expand_dims(absdiff, axis = 0)
@@ -51,6 +64,5 @@ while run:
          absdiff = cv2.putText(absdiff, 'Unsigned', org, font, 
                    fontScale, color, thickness, cv2.LINE_AA)
          FRAME_WINDOW.image(absdiff)
+"""
 
-else:
-    st.markdown("<h4 style='text-align: center; color: gray;'>Bye..</h4>", unsafe_allow_html=True)
