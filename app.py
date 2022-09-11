9a8180fe1ad5ec6194e3538df939c9af13af84b4
Sumesh0105 <113231945+Sumesh0105@users.noreply.github.com>
2022-09-11 16:21:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,29 +26,21 @@ while run:
 
     return_value, frame2 = camera.read()
     time.sleep(0.001)
-    
-
 
     cv2.imwrite("image1.png", frame1)
     image1 = cv2.imread("image1.png",0)
-    #image1 = cv2.cvtColor(frame1, cv2.IMREAD_COLOR)
-    #image1 = cv2.imdecode(frame1, cv2.IMREAD_COLOR)
+
     #image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image1 = cv2.resize(image1,(256,256))
     image1 = np.dstack([image1]*3)
     cv2.imwrite("image2.png", frame2)
     image2 = cv2.imread("image2.png",0)
-    #image2 = cv2.cvtColor(frame2, cv2.IMREAD_COLOR)
-    #image2 = cv2.imdecode(frame2, cv2.IMREAD_COLOR)
+
     #image2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
     image2 = cv2.resize(image2,(256,256))
     image2 = np.dstack([image2]*3)
 
-
-
-
     absdiff = cv2.absdiff(image1,image2)
-    FRAME_WINDOW.image(absdiff)
     absdiff1 = np.expand_dims(absdiff, axis = 0)
     val = model2.predict(absdiff1)
     if val == 0:
@@ -60,6 +52,5 @@ while run:
                    fontScale, color, thickness, cv2.LINE_AA)
          FRAME_WINDOW.image(absdiff)
 
-    
 else:
     st.markdown("<h4 style='text-align: center; color: gray;'>Bye..</h4>", unsafe_allow_html=True)
